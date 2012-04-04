97ba58bce6d60a40a2e91439b09250686c9f08fe
Michael Arnold <github@razorsedge.org>
2012-04-03 23:23:11 -0700

    Massive update to unit tests.
    Finally got the rspec tests figured out.

--- spec/classes/vmwaretools_spec.rb
+++ spec/classes/vmwaretools_spec.rb
@@ -16,107 +16,108 @@ describe 'vmwaretools' do
     end
   end
 
+  redhatish = ['RedHat', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'PSBM', 'OracleLinux', 'OVS', 'OEL']
+  fedoraish = ['Fedora']
+  suseish   = ['SLES', 'SLED', 'OpenSuSE', 'SuSE']
+
   describe 'on a supported osfamily, non-vmware platform' do
-    let(:params) {{}}
-    let :facts do {
-      :osfamily => 'Redhat',
-      :virtual  => 'foo'
-    }
+    (['RedHat', 'SuSE']).each do |osf|
+      describe "for osfamily #{osf}" do
+        let(:params) {{}}
+        let :facts do {
+          :osfamily => osf,
+          :virtual  => 'foo'
+        }
+        end
+        it { should_not contain_yumrepo('vmware-tools') }
+        it { should_not contain_package('vmware-tools') }
+        it { should_not contain_exec('vmware-uninstall-tools') }
+        it { should_not contain_exec('vmware-uninstall-tools-local') }
+        it { should_not contain_service('vmware-tools') }
+      end
     end
-    it { should_not contain_yumrepo('vmware-tools') }
-    it { should_not contain_package('vmware-tools') }
-    it { should_not contain_exec('vmware-uninstall-tools') }
-    it { should_not contain_exec('vmware-uninstall-tools-local') }
-    it { should_not contain_service('vmware-tools') }
   end
 
   describe 'on a supported osfamily, vmware platform' do
-    let(:params) {{}}
-    let :facts do {
-      :osfamily => 'Redhat',  #TODO
-      :virtual  => 'vmware'
-    }
+    (['RedHat', 'SuSE']).each do |osf|
+      describe "for osfamily #{osf}" do
+        let(:params) {{}}
+        let :facts do {
+          :osfamily => osf,
+          :virtual  => 'vmware'
+        }
+        end
+        it 'should remove Package[VMwareTools]' do
+          should contain_package('VMwareTools').with_ensure('absent')
+        end
+        it { should contain_exec('vmware-uninstall-tools').with_command('/usr/bin/vmware-uninstall-tools.pl') }
+        it { should contain_exec('vmware-uninstall-tools-local').with_command('/usr/local/bin/vmware-uninstall-tools.pl') }
+        it { should contain_service('vmware-tools').with_pattern('vmtoolsd')  }
+      end
     end
-
-    it 'should remove Package[VMwareTools]' do
-      should contain_package('VMwareTools').with_ensure('absent')
-    end
-    #it { should contain_package('vmware-tools').with_name('vmware-tools-nox') }
-    it { should contain_exec('vmware-uninstall-tools').with_command('/usr/bin/vmware-uninstall-tools.pl') }
-    it { should contain_exec('vmware-uninstall-tools-local').with_command('/usr/local/bin/vmware-uninstall-tools.pl') }
-    it { should contain_service('vmware-tools') }
-
-    #redhatish = ['RedHat']
-    redhatish = ['RedHat', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'PSBM', 'OracleLinux', 'OVS', 'OEL']
-    fedoraish = ['Fedora']
-    suseish = ['SLES', 'SLED', 'OpenSuSE', 'SuSE']
 
     redhatish.each do |os|
       describe "for operating system #{os}" do
-        let(:params) {{}}
+        let(:params) {{ :tools_version => '3.5u3' }}
         let :facts do {
-          :virtual => 'vmware',
-          :osfamily => 'RedHat',
-#          :operatingsystemrelease => '6',
-          :operatingsystem => os
+          :virtual                => 'vmware',
+          :osfamily               => 'RedHat',
+          :operatingsystemrelease => '6.1',
+          :architecture           => 'i386',
+          :operatingsystem        => os
         }
         end
-
-        it { should contain_yumrepo('vmware-tools') }
+        it { should contain_yumrepo('vmware-tools').with(
+          :descr    => 'VMware Tools 3.5u3 - RHEL6 i686',
+          :enabled  => '1',
+          :gpgcheck => '1',
+          :gpgkey   => 'http://packages.vmware.com/tools/VMWARE-PACKAGING-GPG-KEY.pub',
+          :baseurl  => 'http://packages.vmware.com/tools/esx/3.5u3/rhel6/i686/',
+          :priority => '50',
+          :protect  => '0'
+        )}
         it { should contain_package('vmware-tools').with_name('vmware-tools-nox') }
       end
     end
 
-    describe 'for operating system Fedora' do
-      let(:params) {{}}
-      let :facts do {
-        :virtual => 'vmware',
-        :osfamily        => 'Redhat',
-        :operatingsystem => 'Fedora'
-      }
+    fedoraish.each do |os|
+      describe "for operating system #{os}" do
+        #let(:params) {{ :tools_version => '4.1latest' }}
+        let :facts do {
+          :virtual                => 'vmware',
+          :osfamily               => 'Redhat',
+          :operatingsystemrelease => '16',
+          :architecture           => 'x86_64',
+          :operatingsystem        => os
+        }
+        end
+        it { should_not contain_yumrepo('vmware-tools') }
+        it { should contain_package('vmware-tools').with_name('open-vm-tools') }
       end
-      it { should_not contain_yumrepo('vmware-tools') }
-      it { should contain_package('vmware-tools').with_name('open-vm-tools') }
     end
 
-#    describe 'on redhat based os' do
-#      let :facts do {
-#        :osfamily        => 'Redhat',
-#        :operatingsystem => 'Redhat'
-#      }
-#      end
-#      it { should contain_yumrepo('vmware-tools') }
-#      it { should contain_yumrepo('vmware-tools').with(
-#        :desc => 'VMware Tools 4.1latest - RHEL6 x86_64',
-#        :enabled => '1',
-#        :gpgcheck => '1',
-#        :gpgkey => 'http://packages.vmware.com/tools/VMWARE-PACKAGING-GPG-KEY.pub',
-#        :baseurl => 'http://packages.vmware.com/tools/esx/4.1latest/rhel6/x86_64/',
-#        :priority => '50',
-#        :protect => '0'
-#      )}
-#    end
-#
-#    describe 'on suse os' do
-#      let :facts do
-#        {:osfamily => 'SuSE'}
-#      end
-#      it { should contain_yumrepo('vmware-tools') }
-##      it { should contain_yumrepo('vmware-tools').with(
-##        :desc => "VMware Tools ${vmwaretools_esx_version_real} - SUSE${majdistrelease} ${yum_basearch}",
-##        :enabled => '1',
-##        :gpgcheck => '1',
-##        :gpgkey => "${vmwaretools::params::yum_server}${vmwaretools::params::yum_path}/VMWARE-PACKAGING-GPG-KEY.pub",
-##        :baseurl => "${vmwaretools::params::yum_server}${vmwaretools::params::yum_path}/esx/${vmwaretools_esx_version_real}/suse${majdistrelease}/${yum_basearch}/",
-##        :priority => '50',
-##        :protect => '0'
-##      )}
-#    end
-
+    suseish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) {{ :tools_version => '4.0latest' }}
+        let :facts do {
+          :virtual                => 'vmware',
+          :osfamily               => 'SuSE',
+          :operatingsystemrelease => '10',
+          :architecture           => 'i386',
+          :operatingsystem        => os
+        }
+        end
+        it { should contain_yumrepo('vmware-tools').with(
+          :descr    => 'VMware Tools 4.0latest - SUSE10 i586',
+          :enabled  => '1',
+          :gpgcheck => '1',
+          :gpgkey   => 'http://packages.vmware.com/tools/VMWARE-PACKAGING-GPG-KEY.pub',
+          :baseurl  => 'http://packages.vmware.com/tools/esx/4.0latest/suse10/i586/',
+          :priority => '50',
+          :protect  => '0'
+        )}
+        it { should contain_package('vmware-tools').with_name('vmware-tools-nox') }
+      end
+    end
   end
-
 end
-#    it { should contain_package('vmware-tools').with(
-#      :name => 'vmware-tools-nox',
-#      :ensure => 'latest'
-#    )}
