ee062886ba076c891a30e0d4b40dc069768733ac
Geoff Davis <gadavis@ucsd.edu>
2012-07-08 22:48:59 -0700

    Change ntp test to check for vmwaretools base class
    Existing tests updated to declare the base class as a precondition

--- spec/classes/vmwaretools_ntp_spec.rb
+++ spec/classes/vmwaretools_ntp_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe 'vmwaretools::ntp' do
+  describe 'without base class defined, vmware platform' do
+    let(:params) {{}}
+    let :facts do {
+      :virtual => 'vmware',
+    }
+    end
+    it 'should fail' do
+      expect {
+        subject
+      }.to raise_error(/The class vmwaretools must be declared/)
+    end
+  end
+
+  describe 'without base class defined, non-vmware platform' do
+    let(:params) {{}}
+    let :facts do {
+      :virtual => 'foo',
+    }
+    end
+    it { should_not contain_exec('vmware-tools.syncTime') }
+  end
+
   describe 'on a non-supported osfamily' do
+    let :pre_condition do
+      "class { 'vmwaretools': }"
+    end
     let(:params) {{}}
     let :facts do {
       :osfamily        => 'foo',
@@ -9,13 +34,16 @@ describe 'vmwaretools::ntp' do
     }
     end
     it 'should fail' do
-      expect do
+      expect {
         subject
-      end.should raise_error(/Unsupported platform: foo/)
+      }.to raise_error(Puppet::Error, /Unsupported platform: foo/)
     end
   end
 
   describe 'on a supported osfamily, non-vmware platform' do
+    let :pre_condition do
+      "class { 'vmwaretools': }"
+    end
     (['RedHat', 'SuSE']).each do |osf|
       describe "for osfamily #{osf}" do
         let(:params) {{}}
@@ -30,6 +58,9 @@ describe 'vmwaretools::ntp' do
   end
 
   describe 'on a supported osfamily, vmware platform' do
+    let :pre_condition do
+      "class { 'vmwaretools': }"
+    end
     (['RedHat', 'SuSE']).each do |osf|
       describe "for osfamily #{osf}" do
         let(:params) {{}}
